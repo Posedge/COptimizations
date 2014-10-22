d9b70bb1aa08bcbbfd257f27b3b0058c892b1f51
Posedge <Posedge@users.noreply.github.com>
2014-10-22 14:47:02 +0200

    added matrix multiplication test

---
+++ Nested vs Multilevel Arrays/matrix_multiplication.c
@@ -0,0 +1,96 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <assert.h>
+
+// Too large values here will result in a segmentation fault
+#define MSIZE 800
+
+int main(){
+  // Matrices as nested arrays
+  int nested1[MSIZE][MSIZE];
+  int nested2[MSIZE][MSIZE];
+  int nestedresult[MSIZE][MSIZE];
+
+  // As multi-level arrays
+  int **multi1;
+  multi1 = (int **)malloc(MSIZE * sizeof(int *));
+  int **multi2 = (int **)malloc(MSIZE * sizeof(int *));
+  int **multiresult = (int **)malloc(MSIZE * sizeof(int *));
+  for(int i = 0; i < MSIZE; ++i){
+    multi1[i] = (int *)malloc(MSIZE * sizeof(int));
+    multi2[i] = (int *)malloc(MSIZE * sizeof(int));
+    multiresult[i] = (int *)malloc(MSIZE * sizeof(int));
+  }
+
+  // As dynamically allocated 'nested' arrays
+  int *dyn1 = (int *)malloc(MSIZE * MSIZE * sizeof(int));
+  int *dyn2 = (int *)malloc(MSIZE * MSIZE * sizeof(int));
+  int *dynresult = (int *)malloc(MSIZE * MSIZE * sizeof(int));
+
+  // Fill with random values
+  srand(time(NULL)); // randomize
+  for(int i = 0; i < MSIZE; ++i){
+    for(int j = 0; j < MSIZE; ++j){
+      dyn1[MSIZE*i + j] = multi1[i][j] = nested1[i][j] = rand();
+      dyn2[MSIZE*i + j] = multi2[i][j] = nested2[i][j] = rand();
+    }
+  }
+
+  // Time matrix multiplication
+  clock_t start_nested, end_nested, start_multi, end_multi, start_dyn, end_dyn;
+
+  start_nested = clock();
+  // Not surre if putting this in a function is going to convert one array type to something else
+  // will try like this first
+  for(int m = 0; m < MSIZE; ++m){
+    for(int n = 0; n < MSIZE; ++n){
+      nestedresult[m][n] = 0;
+      for(int i = 0; i < MSIZE; i++){
+        nestedresult[m][n] += nested1[m][i] * nested2[i][n];
+        // repeatedly accessing the same array element
+      }
+    }
+  }
+  end_nested = clock();
+  printf("nested matrix multiplication of size %dx%d took %f milliseconds.\n", MSIZE, MSIZE, (double)(end_nested - start_nested) / CLOCKS_PER_SEC);
+
+  start_multi = clock();
+  for(int m = 0; m < MSIZE; ++m){
+    for(int n = 0; n < MSIZE; ++n){
+      multiresult[m][n] = 0;
+      for(int i = 0; i < MSIZE; i++){
+        multiresult[m][n] += multi1[m][i] * multi2[i][n];
+      }
+    }
+  }
+  end_multi = clock();
+  printf("multilevel matrix multiplication of size %dx%d took %f milliseconds.\n", MSIZE, MSIZE, (double)(end_multi - start_multi) / CLOCKS_PER_SEC);
+
+  start_dyn = clock();
+  for(int m = 0; m < MSIZE; ++m){
+    for(int n = 0; n < MSIZE; ++n){
+      dynresult[MSIZE * m + n] = 0;
+      for(int i = 0; i < MSIZE; i++){
+        dynresult[MSIZE * m + n] += dyn1[MSIZE * m + i] * dyn2[MSIZE * i + n];
+      }
+    }
+  }
+  end_dyn = clock();
+  printf("dynamic array matrix multiplication of size %dx%d took %f milliseconds.\n", MSIZE, MSIZE, (double)(end_dyn - start_dyn) / CLOCKS_PER_SEC);
+
+  for(int m = 0; m < MSIZE; ++m){
+    for(int n = 0; n < MSIZE; ++n){
+      int result = nestedresult[m][n];
+      assert(result == multiresult[m][n]);
+      assert(result = dynresult[MSIZE * m + n]);
+    }
+  }
+
+  for(int i = 0; i < MSIZE; ++i){
+    free(multi1[i]); free(multi2[i]); free(multiresult[i]);
+  }
+  free(multi1); free(multi2); free(multiresult);
+  free(dyn1); free(dyn2); free(dynresult);
+  return 0;
+}
--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-COptimizations
-==============
+## COptimizations
 
-Some C99 Timing benchmarks and optimizations
+Some timing benchmarks of a few C99 codes which I come across throughout my Systems Programming and Computer Architecture course at the university.
+
+### Nested vs Multilevel Arrays
+multi-dimensional arrays in C can be nested (basically converting them to a longer one-dimensional array,
+and having the data stored consecutively in memory with indexing offsets)
+or they can be java-like arrays of arrays, meaning an array which contains pointers (arrays).
+The latter should, in theory, be a lot slower. Accessing an element in a nested array requires only a single memory load/store,
+in addition there should be optimizations in the processor design which allow caching of elements likely to be loaded, when traversing a two-dimensional array in any direction.
+A two-dimensional multilevel-array on the other hand requires two memory loads every time.
+A third way is to store everything in a regular dynamically allocated array, and offset column indizes by row length * row index. This should be a little slower than nested
+arrays, as the compiler cannot optimize the index and has to perform a regular multiplication every time.
+With this code you can compare the speed of the three methods. It also heavily depends on the optimization level.
